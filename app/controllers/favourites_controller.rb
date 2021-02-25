ac3960b3acb44a9dde3d84347e1f182103904982
Alexandra Canuel <alexcanuel@gmail.com>
2021-02-25 14:01:10 +0100

    create favourites

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -13,11 +13,11 @@ class FavouritesController < ApplicationController
     @favourite = Favourite.new(favourite_params)
     @user = User.find(params[:user_id])
     @favourite.user = @user
-    if @favourite.save
-      redirect_to_dashboard_path
-    else
-      render :new
-    end
+      if @favourite.save
+        redirect_to_dashboard_path
+      else
+        render :new
+      end
   end
 
   private
