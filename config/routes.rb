7a4ba8aa8e81c91aa055ea42c4b75c78b09f356e
Edouard Belfond <edouard.belfond@grenoble-em.com>
2021-03-04 12:26:29 +0100

    error 404, 500 page landing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   devise_for :users
   root to: 'pages#home'
   get 'onboarding', to:"pages#onboarding"
   get 'dashboard', to:"pages#dashboard"
   get "update-available-places", to: "parking_spots#update_available_places"
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :parking_spots do
